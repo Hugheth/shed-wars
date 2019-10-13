25d3c59ad99a49830f0b939ec4073bc328075709
Hugheth <thehugheth@gmail.com>
2019-10-13 15:43:49 +0100

    Stairs work vaguely

--- src/GameKits/Map Drawers/BuildingDrawer/ReplicatedStorage/BuildingDrawer.lua
+++ src/GameKits/Map Drawers/BuildingDrawer/ReplicatedStorage/BuildingDrawer.lua
@@ -30,6 +30,8 @@ function BuildingDrawer.drawBuilding(options)
 	assert(type(floorHeight) == "number" and floorHeight > 0, "The floorHeight must be a positive integer")
 	assert(type(name) == "string", "Name must be a string")
 
+	options.stairs = options.stairs or BuildingDrawer.pickStairs(options)
+
 	local folder = Instance.new("Folder")
 	folder.Name = name
 
@@ -63,6 +65,25 @@ function BuildingDrawer.drawBuilding(options)
 	return folder
 end
 
+function BuildingDrawer.pickStairs(options)
+	local stairs = {}
+	local downStairsPosition
+	local stairPosition
+	for floorNumber = 1, #options.floors - 1 do
+		local floor = options.floors[floorNumber]
+		if floor.sizeX > 1 and floor.sizeY > 1 then
+			while stairPosition == downStairsPosition do
+				local x = options.random:NextInteger(1, floor.sizeX)
+				local y = options.random:NextInteger(1, floor.sizeY)
+				stairPosition = Vector2.new(x, y)
+			end
+			table.insert(stairs, stairPosition)
+			downStairsPosition = stairPosition
+		end
+	end
+	return stairs
+end
+
 function BuildingDrawer.drawRoom(options, floorNumber, x, y)
 	local folder = Instance.new("Folder")
 	folder.Name = "Room (" .. x .. "," .. y .. ")"
@@ -80,9 +101,11 @@ function BuildingDrawer.drawRoom(options, floorNumber, x, y)
 	end
 
 	local isFilled = options.random:NextNumber() < options.tileFillChance and not isHorizontalDoor and not isVerticalDoor
-	local roomTile, ceilingTile = BuildingDrawer.drawTile(options, roomPosition, isFilled)
+	local roomTile, ceilingTile = BuildingDrawer.drawTile(options, floorNumber, x, y, roomPosition, isFilled)
 	roomTile.Parent = folder
-	ceilingTile.Parent = folder
+	if ceilingTile then
+		ceilingTile.Parent = folder
+	end
 
 	return folder
 end
@@ -144,15 +167,31 @@ function BuildingDrawer.drawVerticalWall(options, floorNumber, x, y, roomPositio
 	return verticalWall, isDoor
 end
 
-function BuildingDrawer.drawTile(options, roomPosition, isFilled)
+function BuildingDrawer.drawTile(options, floorNumber, x, y, roomPosition, isFilled)
 	-- Pick a tile
 	local templateName = isFilled and "Decoration" or "Corridor"
+	local hasCeiling = true
+
+	local stairs = options.stairs[floorNumber]
+	if stairs and stairs.X == x and stairs.Y == y then
+		templateName = "Stairs"
+		hasCeiling = false
+	end
+	local upStairs = options.stairs[floorNumber - 1]
+	if upStairs and upStairs.X == x and upStairs.Y == y then
+		templateName = "Upstairs"
+	end
+
 	local tile = BuildingDrawer.getTileFromTemplate(options.templates, templateName, options.random)
 	tile:SetPrimaryPartCFrame(CFrame.new(roomPosition))
-	-- Pick a ceiling
-	local ceilingTile = BuildingDrawer.getTileFromTemplate(options.templates, "Ceiling", options.random)
-	local ceilingPosition = roomPosition + Vector3.new(0, options.floorHeight - 0.5, 0)
-	ceilingTile:SetPrimaryPartCFrame(CFrame.new(ceilingPosition))
+
+	local ceilingTile
+	if hasCeiling then
+		-- Pick a ceiling
+		ceilingTile = BuildingDrawer.getTileFromTemplate(options.templates, "Ceiling", options.random)
+		local ceilingPosition = roomPosition + Vector3.new(0, options.floorHeight - 0.5, 0)
+		ceilingTile:SetPrimaryPartCFrame(CFrame.new(ceilingPosition))
+	end
 
 	return tile, ceilingTile
 end
